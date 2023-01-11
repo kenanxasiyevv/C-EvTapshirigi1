e03b87761138f4fb7ee672c412aaf0b168d98fc4
kenanxasiyevv <kananakh@code.edu.az>
2023-01-12 03:09:29 +0400

    Add project files.

---
+++ EvTapshirigi1/ArrayTasks.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvTapshirigi1
+{
+    internal class ArrayTasks
+    {
+        internal int[] EnterArray()
+        {
+            Console.Write("Massivin uzunlugunu daxil et: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] mass = new int[n];
+            int r = 0;
+            for (int i = 0; i < mass.Length; i++)
+            {
+                Console.Write($"mass[{i}] = ");
+                mass[i] = int.Parse(Console.ReadLine());
+            }
+            return mass;
+
+        }
+
+     
+        internal int[] SquareRoot()
+        { 
+            int[] mass = EnterArray();
+            for (int i = 0; i < mass.Length; i++)
+            {
+                if (Math.Sqrt(mass[i]) == (int)Math.Sqrt(mass[i]))
+                    Console.WriteLine(mass[i]);
+
+
+
+            }
+                    return mass;
+            
+        } 
+    }
+}
+
---
+++ EvTapshirigi1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace EvTapshirigi1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var Arraytasks = new ArrayTasks();
+
+            var mass = Arraytasks.SquareRoot();
+
+          
+        }
+    }
+}
